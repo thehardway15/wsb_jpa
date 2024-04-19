c7f0066bd1488d4d1e704c66cb7c90501e9fdfff
Damian Wiśniewski <dev@thehardway.pl>
2024-04-19 22:34:58 +0200

    Patient test

--- wsb-jpa1/src/main/test/com/capgemini/wsb/service/PatientServiceTest.java
+++ wsb-jpa1/src/main/test/com/capgemini/wsb/service/PatientServiceTest.java
@@ -1,6 +1,8 @@
 package com.capgemini.wsb.service;
 
+import com.capgemini.wsb.dto.DoctorTO;
 import com.capgemini.wsb.dto.PatientTO;
+import com.capgemini.wsb.dto.VisitTO;
 import com.capgemini.wsb.service.PatientService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -16,6 +18,8 @@ public class PatientServiceTest {
 
     @Autowired
     private PatientService patientService;
+    @Autowired
+    private DoctorService doctorService;
 
     @Transactional
     @Test
@@ -23,10 +27,31 @@ public class PatientServiceTest {
         // given
 
         // when
-        PatientTO patientTO = patientService.findById(1L);
+        final PatientTO patientTO = patientService.findById(1L);
         // then
         assertThat(patientTO).isNotNull();
         assertThat(patientTO.getId()).isEqualTo(1L);
+        assertThat(patientTO.isVerified()).isTrue();
+        assertThat(patientTO.getVisits().size()).isEqualTo(1);
+
+        final VisitTO visit = patientTO.getVisits().iterator().next();
+        assertThat(visit.getMedicalTreatments().size()).isEqualTo(1);
+        assertThat(visit.getDoctor()).isNotNull();
+        assertThat(visit.getPatient()).isNotNull();
+        assertThat(visit.getDoctor().getId()).isEqualTo(1L);
+        assertThat(visit.getPatient().getId()).isEqualTo(1L);
+    }
+
+    @Transactional
+    @Test
+    public void testRemovePatientShouldRemoveVisitSNotPatient() {
+        // given
+        // when
+        patientService.deleteById(3L);
+        // then
+        assertThat(patientService.findAll().size()).isEqualTo(2);
+        final DoctorTO doctorTO = doctorService.findById(3L);
+        assertThat(doctorTO.getVisits().size()).isEqualTo(1);
     }
 
 }
